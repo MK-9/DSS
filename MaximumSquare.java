eac02b574719c0a55f49fbaad86ed5e089a98ddd
soltanmohammadi <msit26055@gmail.com>
2022-12-03 09:14:40 +0330

    implement maximum square by dynamic programming

--- MaximumSquare.java
+++ MaximumSquare.java
@@ -4,12 +4,13 @@ public class MaximumSquare {
         char[][] matrix = new char[][] {
                 { '1', '1', '1', '1', '0' },
                 { '1', '1', '1', '1', '1' },
-                { '1', '1', '1', '0', '1' },
-                { '1', '0', '0', '1', '0' },
+                { '1', '1', '1', '1', '1' },
+                { '1', '1', '1', '1', '0' },
                 { '1', '1', '1', '1', '1' }
         };
 
         System.out.println("maximum square:" + maximalSquareBruteForce(matrix));
+        System.out.println("maximum square:" + maximumSquareDp(matrix));
     }
 
     public static int maximalSquareBruteForce(char[][] matrix) {
@@ -109,6 +110,42 @@ public class MaximumSquare {
         }
 
         return area;
+    }
+
+    public static int maximumSquareDp(char[][] matrix) {
+
+        // DP[i][j] show length in maximum square that is located it's bottomLeft point
+        int[][] DP = new int[matrix.length][matrix[0].length];
+
+        // last side length
+        int mLength = 0;
+
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[0].length; j++) {
+
+                // fill first row and col in DP matrix
+                if (i == 0 || j == 0) {
+                    if (matrix[i][j] == '1') {
+                        // fill DP
+                        DP[i][j] = 1;
+
+                        // update area
+                        if (DP[i][j] > mLength)
+                            mLength = 1;
+                    }
+                } else {
+
+                    if (matrix[i][j] == '1') {
+                        DP[i][j] = Math.min(Math.min(DP[i - 1][j], DP[i][j - 1]), DP[i - 1][j - 1]) + 1;
+                    }
+                    // update area
+                    if (DP[i][j] > mLength)
+                        mLength = DP[i][j];
+                }
+            }
+        }
+
+        return mLength * mLength;
     }
 
 }
